80a67188106f79047b045ff46a786244e85d5852
aleksvvv <adm-shop-001@yandex.ru>
2023-04-22 12:27:16 +0300

    finish dizain

--- app/src/main/java/com/bignerdranch/android/firstcomposeproject/ui/theme/InstagramCard.kt
+++ app/src/main/java/com/bignerdranch/android/firstcomposeproject/ui/theme/InstagramCard.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -15,8 +16,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.textInputServiceFactory
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -33,38 +36,49 @@ fun InstagramCard() {
         border = BorderStroke(1.dp, MaterialTheme.colors.onBackground)
 
     ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp),
-            horizontalArrangement = Arrangement.SpaceEvenly,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-
-            Image(
-                painter = painterResource(
-                    id =
-                    R.drawable.ic_instagramm
-                ),
-                contentDescription = "",
-
-                        modifier = Modifier
-                            .clip(CircleShape)
-                            .size(70.dp)
-                            .background(Color.White)
-                            .padding(9.dp)
-                            .size(50.dp)
-
-
+        Column (modifier = Modifier.padding(16.dp)){
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(
+                        id =
+                        R.drawable.ic_instagramm
+                    ),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .clip(CircleShape)
+                        .size(70.dp)
+                        .background(Color.White)
+                        .padding(9.dp)
+                        .size(50.dp)
+                )
+                TwoText(title = "Posts", count = "6.950")
+                TwoText(title = "Followers", count = "436M")
+                TwoText(title = "Following", count = "76")
+            }
+            Text(
+                text = "Instagram",
+                fontSize = 32.sp,
+                fontFamily = FontFamily.Cursive
             )
-
-            TwoText(title = "Posts", count = "6.950")
-            TwoText(title = "Followers", count = "436M")
-            TwoText(title = "Following", count = "76")
-
+            Text(
+                text = "#YoursToMake",
+            )
+            Text(
+                text = "www.facebook.com/lll",
+            )
+            Button(
+                onClick = {  },
+            ) {
+                Text(text = "Follow")
+            }
         }
-    }
 
+    }
 }
 
 @Composable
